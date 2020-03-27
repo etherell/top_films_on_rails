641873b030069df624c482f7e61b5e42bd0b0585
Synkevych Roman <synkevych.roman@gmail.com>
2020-03-27 17:44:29 +0200

    feat: new functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,41 +6,46 @@ class HomeController < ApplicationController
     require 'json'
 
     # create an instance of API url
-    @top_rated = "top_rated?" 
-    @url = 'https://api.themoviedb.org/3/movie/popular?api_key=f86cc31e3850cd90ef8e189703f28ac1'
+    @top_rated = 'movie/top_rated?';
+    $API_KEY = 'api_key=f86cc31e3850cd90ef8e189703f28ac1';
+    $popular_movies= 'movie/popular?';
+    $SEARCH = 'search/movie?';
+    $img_url = 'https://image.tmdb.org/t/p/w500';
+    $DOMEN = 'https://api.themoviedb.org/3/';
+    @url = $DOMEN + $popular_movies + $API_KEY;
     # uniform resource indicator - URL type thing
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @output = JSON.parse(@response)
-    @img_url = 'https://image.tmdb.org/t/p/w500' 
 
-    
     # # get only first films 
-    # @output = @output['results'][0]
-
     # @title = @output['original_title']
     # @release_date = @output['release_date']
     # @overview = @output['overview']
     # @homepage = @output['homepage']
     # @poster = @img_url + @output['poster_path'].to_s
 
-    # def uptate
-    #   @output = @output['results'].all do
-    #     | film |
-    #      @title = film['original_title']
-    #      @overview = film['overview']
-    #      @homepage = film['homepage']
-    #      @poster = @img_url + film['poster_path'].to_s
-
-    #      render "home/_card"
-    #     end
-    #   end
-
     # check for empty return result 
-    if @output.empty?
-      @final_output = "Error"
+    if @output['results'].empty?
+      @final_output = "Error nothing to render here"
     else
       @final_output = "Top 20 Best Movies This Week"
+      render "home/_card"
+    end
+  end
+  def top_films
+    @film_name = params[:top_films]
+    
+    if params[:top_films] == ""
+      @film_name = "Films is not found!"
+    elsif
+      # Do API stuff
+      @film_name
+      @url = $DOMEN + $SEARCH + $API_KEY + '&query=' + params[:top_films].to_s
+      @uri = URI(@url)
+      @response = Net::HTTP.get(@uri)
+      @output = JSON.parse(@response)
+      render "home/_card"
     end
   end
 end
