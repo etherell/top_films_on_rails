12129854ef3c1b7859eeae670d37b3ee71ae70b7
Synkevych Roman <synkevych.roman@gmail.com>
2020-03-27 17:45:45 +0200

    feat: new post page for searching

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   #this is from template
-  get 'home/index'
+  # get 'home/top_films'
   get 'welcome/index'
 
   resources :articles
-  
+  # for searching 
+  post "top_films" => 'home#top_films'
+
   #we change this to cource
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
