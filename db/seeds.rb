2184c80ca49e3289247f3293756b3042f38c04d8
Synkevych Roman <sunkevu4@gmail.com>
2020-03-31 01:34:03 +0300

    feat: work with export seeds_format

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Country.create(name: 'Belgium', popularion: 10839905)
-Country.create(name: 'Netherlands', popularion: 16680000)
-Country.create(name: 'Ukraine', population: 31753159)
+
+# Country.create(name: 'Belgium', population: 10839905)
+# Country.create(name: 'Netherlands', population: 16680000)
+# Country.create(name: 'Ukraine', population: 31753159)
+
+country_list = [ 
+    [ "Germany", 8183100 ],
+    [ "Russia", 212313124 ],
+    [ "USA", 312312312 ]
+]
+
+country_list.each do |name, population| 
+    Country.create( name: name, population: population)
+end
---
+++ db/seeds2.rb
@@ -0,0 +1,4 @@
+Country.create({"name"=>"Ukraine", "population"=>3423232})
+Country.create({"name"=>"France", "population"=>65447374})
+Country.create({"name"=>"Belgium", "population"=>10839905})
+Country.create({"name"=>"Netherlands", "population"=>8002121})
