b5dfae0a75ea3a0b15c9273cf71d22a670c71d89
Synkevych Roman <sunkevu4@gmail.com>
2020-03-31 23:16:04 +0300

    feat: added create, edit, view func

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,53 @@
+# To save data we need create here some logic 
+
 class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+	
+	def show
+    @article = Article.find(params[:id])
+	end
+	
 	def new
-
+		@article = Article.new
 	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+	
 	def create
-		render plain: params[:aticle].inspect
+		# render method is taking very simple hash with a key :plain 
+		# params - method represent the param coming in from the form 
+		# params return the object 
+	    #render plain: params[:aticle].inspect
+	
+	  @article = Article.new(article_params)
+	  
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+	  
+	def update
+   @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+		end
+	end
+	
+	def destroy 
 	end
 
+	private
+	def article_params
+		params.require(:article).permit(:title, :text)
+	end
+	
 end
--- db/migrate/20200331135603_create_articles.rb
+++ db/migrate/20200331135603_create_articles.rb
@@ -1,3 +1,9 @@
+# This file create automatically 
+# Those attributes are automatically added to the articles table 
+# and mapped to the Article model 
+
+# This file is responsible for creating the database structure, which us what we'll look at next
+
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
