408bb2abe3515688a8ae25153215ee812af0c665
Synkevych Roman <sunkevu4@gmail.com>
2020-04-01 00:04:15 +0300

    fix: change link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   #this is from template when you create welcome controllers 
-  get 'welcome/index'
+  #get 'welcome/index'
 
-  # get 'home/top_films'
+  get 'home/index'
 
   # resource method can be used to declare a standart REST resource
   # you need to add the article resource to the config/routes.rb so the file will look as follows
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   post "searching" => 'home#searching'
 
   #we change this from -> root 'home#index'
-  root 'welcome#index'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
